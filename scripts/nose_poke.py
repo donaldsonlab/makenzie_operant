7cf34c7c9a5b6745421b7bd56c8eaa60f96e6f6b
Ryan Cameron <ryca1116@colorado.edu>
2023-08-02 16:49:43 -0600

    pushing for other pies

--- scripts/nose_poke.py
+++ scripts/nose_poke.py
@@ -14,8 +14,8 @@ def run():
     box.setup(run_dict=RUNTIME_DICT, 
               user_hardware_config_file_path=USER_HARDWARE_CONFIG_PATH,
               user_software_config_file_path=USER_SOFTWARE_CONFIG_PATH,
-              start_now=True, simulated = False)
-    phase = box.timing.new_phase('setup_phase', length = 5)
+              start_now=True, simulated = False, verbose = True)
+    phase = box.timing.new_phase('setup_phase', length = 3)
     box.reset()
     
     #simplifying hardware calls
@@ -28,16 +28,21 @@ def run():
     FR = box.get_software_setting(location = 'values', setting_name='FR', default = 1)
     #start beam break monitoring. calculate durations
     box.beams.door1_ir.start_getting_beam_broken_durations() 
-    box.beams.door2_ir.start_getting_beam_broken_durations() 
-    phase.wait()
-    poke_1.begin_monitoring()
-    poke_2.begin_monitoring()
+    box.beams.door2_ir.start_getting_beam_broken_durations()
+    reward_phase_d2 = box.timing.new_phase('reward_phase',length = 0.05)
+    reward_phase_d1 = box.timing.new_phase('reward_phase',length = 0.05)
     
+    
+    phase.wait()
     total_time = box.get_software_setting(location = 'values', setting_name = 'experiment_length', default = 30*60)
     
     #wait to finish setup
     
     box.timing.new_round()
+
+    poke_1.begin_monitoring()
+    poke_2.begin_monitoring()
+
     total_time_phase = box.timing.new_phase('experiment', length = total_time)
     
     poke_1.set_poke_target(FR)
@@ -47,23 +52,29 @@ def run():
         
         if door_1.is_open():
             if not reward_phase_d1.active():
-                door_1.close()
+                door_1.close(wait = True)
                 poke_2.set_poke_target(FR)
         
         if door_2.is_open():
             if not reward_phase_d2.active():
-                door_2.close()
+                door_2.close(wait = True)
                 poke_1.set_poke_target(FR)
         
         if poke_1.pokes_reached:
             
+            speaker.play_tone(tone_name = 'door_2_open', wait = True)
+            timeout = box.timing.new_timeout(length = delay)
+            timeout.wait()
             door_2.open()
-            reward_phase_d2 = box.timing.new_phase('reward_phase',length = box.software_config['values']['reward_length'])
+            reward_phase_d2 = box.timing.new_phase('reward_phase_d2',length = box.software_config['values']['reward_length'])
             poke_1.reset_poke_count()
             
         if poke_2.pokes_reached:
+            speaker.play_tone(tone_name = 'door_1_open', wait = True)
+            timeout = box.timing.new_timeout(length = delay)
+            timeout.wait()
             door_1.open()
-            reward_phase_d1 = box.timing.new_phase('reward_phase',length = box.software_config['values']['reward_length'])
+            reward_phase_d1 = box.timing.new_phase('reward_phase_d1',length = box.software_config['values']['reward_length'])
             poke_2.reset_poke_count()
             
     box.shutdown()
